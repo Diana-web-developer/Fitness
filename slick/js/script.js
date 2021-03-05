11eddff594566e834e27681864f1bb8bfd1878c3
Diana-web-developer <71963634+Diana-web-developer@users.noreply.github.com>
2021-03-05 13:55:27 +0300

    Add files via upload

---
+++ slick/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	
+
+
+	$('.slider-three-section').slick({
+		arrows: true,
+		dots: false,
+		slidesToShow: 1,
+		autoplay: false,
+		speed: 1000,
+		autoplaySpeed: 800,
+		fade: true,
+	});
+
+	
+
+});
+
